5b67a3305f2133b8dfab6082ed0c9e1d31fdb348
amandaltr <amandalumbantoruan@gmail.com>
2024-04-23 11:06:27 +0700

    Tugas 3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+<footer class="footer footer-center p-4 bg-base-300 text-base-content" style="position: fixed; bottom: 0; width: 100%;">
     <aside>
       <p>Copyright © 2024 - All right reserved by Amanda Lumbantoruan</p>
     </aside>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TodoTaskController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [TodoTaskController::class, 'index']);
+
+Route::post('/', [TodoTaskController::class, 'store']);
+
+Route::delete('/{id}', [TodoTaskController::class, 'destroy']);
